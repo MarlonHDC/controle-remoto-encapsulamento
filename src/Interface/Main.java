c0ec1b2a0bd64e58a99f49e427d74ed6a5790da1
origin <https://github.com/MarlonHDC/teste-curso-dio-dominando-ides-java-intellij.git>
2021-12-23 13:05:01 -0300

    Initial commit

---
+++ src/Interface/Main.java
@@ -0,0 +1,12 @@
+package Interface;
+
+public class Main {
+
+    public static void main(String[] args) {
+	ControleRemoto c = new ControleRemoto();
+    c.ligar();
+    c.abrirMenu();
+    c.play();
+    c.maisVolume();
+    }
+}
